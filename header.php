1c00b6d5934eb011d70bb4f5b35e63054c56d63d
wombeez <jpeck@intuitsolutions.net>
2018-01-03 21:45:10 -0500

    impememented custom page template

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
@@ -31,11 +31,8 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
+          <a class="blog-nav-item active" href="<?php echo get_bloginfo( 'wpurl' );?>">Home</a>
+          <?php wp_list_pages( '&title_li=' ); ?>
         </nav>
       </div>
     </div>
@@ -43,6 +40,8 @@
     <div class="container">
 
       <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <a href="<?php echo get_bloginfo( 'wpurl' );?>">
+          <h1 class="blog-title"><?php echo get_bloginfo( 'name' ); ?></h1>
+        </a>
+        <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
       </div>
